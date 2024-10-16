80146cdd0cb4939f097db9e56d1a7501106a17fb
WarleySeidelll <warley.s@edu.udesc.br>
2024-10-16 10:34:42 -0300

    Commit Carro

--- src/exercicios3oo/Carro.java
+++ src/exercicios3oo/Carro.java
@@ -1,26 +1,50 @@
 package exercicios3oo;
 
 public class Carro {
+    private String modelo;
+    private double velocidade = 0;
 
-        private String modelo;
-        private double velocidade;
-
-        public void setModelo(String modelocarro,double velocidadeatual){
-            this.modelo= modelocarro;
-            this.velocidade = velocidadeatual;
-        }
-        public double getAcelerar(double aumentar){
-
-            return velocidade += aumentar;
-        }
-        public double getFreiar(double diminuir){
-            System.out.println("Você Freiou ");
-            return velocidade -= diminuir;
-        }
-
-        public void getImprimir(){
-            System.out.println("o modelo do carro é:"+modelo);
-            System.out.println("a sua velocidade é:"+velocidade);
-        }
+    public Carro(){
+        this.modelo = "";
     }
+
+    public Carro(String modelo, double velocidade){
+        this();
+        velocidade = 0;
+        this.modelo=modelo;
+        this.velocidade=velocidade;
+
+    }
+    public void acelerar(){
+        velocidade += 10;
+        System.out.printf("\nAcelerando...");
+    }
+    public void freiar(){
+        velocidade -= 10;
+        System.out.printf("\nFreando...");
+    }
+    public void exibirvelocidadeAtual() {
+        System.out.printf("\nSua velocidade atual é %.0f Km/h",getvelocidade() );
+    }
+
+    @Override
+    public String toString() {
+           return "Carro{" +
+                   "modelo=" + modelo +
+                   "velocidade=" + velocidade +
+                   '}';
+    }
+    public String getModelo() {
+        return modelo;
+    }
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+    public double getvelocidade() {
+        return velocidade;
+    }
+    public void setvelocidade(double velocidade) {
+        this.velocidade = velocidade;
+    }
+}
 
--- src/exercicios3oo/Testecarro.java
+++ src/exercicios3oo/Testecarro.java
@@ -1,4 +1,15 @@
 package exercicios3oo;
 
 public class Testecarro {
+
+    public static void main(String[] args) {
+
+        Carro carro = new Carro("Chevette",0);
+        carro.acelerar();
+        carro.freiar();
+        carro.exibirvelocidadeAtual();
+        carro.acelerar();
+        carro.acelerar();
+        carro.exibirvelocidadeAtual();
+    }
 }
