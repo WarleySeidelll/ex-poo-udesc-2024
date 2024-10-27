382e2522ff8663aa68d39ef592eecb299e21eab9
Warley Seidel <warley.s@edu.udesc.br>
2024-10-27 13:11:42 -0300

    Indentação Retangulo

--- src/exercicios3oo/Retangulo.java
+++ src/exercicios3oo/Retangulo.java
@@ -4,26 +4,28 @@ public class Retangulo {
     private double ld1;
     private double ld2;
 
-    public Retangulo(){}
+    public Retangulo() {
+    }
 
-    public Retangulo(double ld1, double ld2){
+    public Retangulo(double ld1, double ld2) {
         this();
         this.ld1 = ld1;
         this.ld2 = ld2;
     }
 
     @Override
-    public String toString(){
-        return  "Lado 1 : " + ld1 +
+    public String toString() {
+        return "Lado 1 : " + ld1 +
                 "\n" +
                 "Lado 2 : " + ld2;
     }
 
-    public double calcularArea(){
+    public double calcularArea() {
         return this.getLd1() * this.getLd2();
     }
-    public void imprimirArea(){
-        if(ld1 > 0 && ld2 > 0) {
+
+    public void imprimirArea() {
+        if (ld1 > 0 && ld2 > 0) {
             System.out.printf("A área do retângulo é %.2f: ", calcularArea());
         } else if (ld1 < 0 || ld2 < 0) {
             System.out.printf("Digite um valor válido para calcular a Área!!!");
@@ -33,15 +35,19 @@ public class Retangulo {
             System.exit(0);
         }
     }
+
     public double getLd1() {
         return ld1;
     }
+
     public double getLd2() {
         return ld2;
     }
+
     public void setLd1(double ld1) {
         this.ld1 = ld1;
     }
+
     public void setLd2(double ld2) {
         this.ld2 = ld2;
     }
--- src/exercicios3oo/TesteRetangulo.java
+++ src/exercicios3oo/TesteRetangulo.java
@@ -3,15 +3,15 @@ package exercicios3oo;
 import java.util.Scanner;
 
 public class TesteRetangulo {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
 
         Retangulo ret = new Retangulo();
-        System.out.printf("Digite o valor  1º lado do retângulo : ");
+        System.out.printf("Digite o valor do 1º lado do retângulo : ");
         ret.setLd1(sc.nextDouble());
 
-        System.out.printf("Digite o valor  2º lado do retângulo : ");
+        System.out.printf("Digite o valor do 2º lado do retângulo : ");
         ret.setLd2(sc.nextDouble());
 
 
