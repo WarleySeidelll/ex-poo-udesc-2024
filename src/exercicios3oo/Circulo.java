3fb60ae22cf8cd3b3ef7b540acf1d12878191fe3
Warley Seidel <warley.s@edu.udesc.br>
2024-10-27 11:12:35 -0300

    Commit Circulo

--- src/exercicios3oo/Circulo.java
+++ src/exercicios3oo/Circulo.java
@@ -1,4 +1,45 @@
 package exercicios3oo;
 
 public class Circulo {
+    private double raio;
+
+
+    public Circulo() {}
+
+    public Circulo(double raio) {
+    this();
+    this.raio = raio;
+}
+public void verificarRaio() {
+        if (raio > 0){
+
+        }else if (raio < 0) {
+            System.out.println("Raio negativo!!");
+        System.exit(0);
+    }
+}
+
+    public double calcularPerimetro() {
+            return 2*Math.PI*getRaio();
+    }
+    public double calcularDiametro() {
+        return 2*getRaio();
+
+    }
+
+    public double calcularArea() {
+        return Math.PI*Math.pow(raio,2);
+    }
+
+    @Override
+    public String toString() {
+    return "\nRaio=" + raio ;
+    }
+
+    public double getRaio() {
+        return this.raio;
+    }
+    public void setRaio(double raio) {
+        this.raio = raio;
+    }
 }
--- src/exercicios3oo/TesteCirculo.java
+++ src/exercicios3oo/TesteCirculo.java
@@ -1,4 +1,40 @@
 package exercicios3oo;
 
+import java.util.Scanner;
+
 public class TesteCirculo {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        Circulo c1 = new Circulo();
+        int opcao = 0;
+
+        System.out.printf(" Informe o raio do circulo: ");
+        c1.setRaio(sc.nextDouble());
+        c1.verificarRaio();
+
+        do{
+        System.out.printf(" \nO que deseja fazer : ");
+            System.out.printf("\n1 | Calcular Diametro");
+            System.out.printf("\n2 | Calcular Area");
+            System.out.printf("\n3 | Calcular Perimetro");
+            System.out.printf("\n4 | Imprimir informações");
+            System.out.printf("\n0 | Sair");
+            System.out.printf("\nDigite o código : ");
+            opcao = sc.nextInt();
+            switch(opcao){
+                case 1:
+                  System.out.printf("Diametro : %.2f",c1.calcularDiametro());
+                    break;
+                    case 2:
+                        System.out.printf("Area : %.2f",c1.calcularArea());
+                        break;
+                        case 3:
+                            System.out.printf("Area : %.2f",c1.calcularPerimetro());
+                            break;
+                            case 4:
+                                System.out.printf(c1.toString());
+            }
+    }while(opcao != 0);
+    sc.close();
+    }
 }
