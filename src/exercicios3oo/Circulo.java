3bfbd3d70debefaafc633ef426fcc2f3bdcaa62d
Warley Seidel <warley.s@edu.udesc.br>
2024-10-27 13:16:13 -0300

    Indentação Circulo

--- src/exercicios3oo/Circulo.java
+++ src/exercicios3oo/Circulo.java
@@ -4,43 +4,47 @@ public class Circulo {
     private double raio;
 
 
-    public Circulo() {}
+    public Circulo() {
+    }
 
     public Circulo(double raio) {
-    this();
-    this.raio = raio;
-}
-public void verificarRaio() {
-        if (raio > 0){
-
-        }else if (raio < 0) {
-            System.out.println("Raio negativo!!");
-        System.exit(0);
+        this();
+        this.raio = raio;
     }
-}
+
+    public void verificarRaio() {
+        if (raio > 0) {
+
+        } else if (raio < 0) {
+            System.out.println("Raio negativo!!");
+            System.exit(0);
+        }
+    }
 
     public double calcularPerimetro() {
-            return 2*Math.PI*getRaio();
+        return 2 * Math.PI * getRaio();
 
 
     }
+
     public double calcularDiametro() {
-        return 2*getRaio();
+        return 2 * getRaio();
 
     }
 
     public double calcularArea() {
-        return Math.PI*Math.pow(raio,2);
+        return Math.PI * Math.pow(raio, 2);
     }
 
     @Override
     public String toString() {
-    return "\nRaio=" + raio ;
+        return "\nRaio=" + raio;
     }
 
     public double getRaio() {
         return this.raio;
     }
+
     public void setRaio(double raio) {
         this.raio = raio;
     }
